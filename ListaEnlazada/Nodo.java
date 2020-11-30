32535b6df8cd7ef0caddd435d7cf5be5625d38bc
AngelMelendez2020 <71795575+AngelMelendez2020@users.noreply.github.com>
2020-11-29 22:27:41 -0600

    Create Nodo.java

---
+++ ListaEnlazada/Nodo.java
@@ -0,0 +1,11 @@
+package ListaEnlazada;
+
+
+public class Nodo {
+Object elemento;
+    Nodo siguiente;
+    public Nodo(Object elemento){
+        this.elemento=elemento;
+        this.siguiente=null;
+    }
+}
